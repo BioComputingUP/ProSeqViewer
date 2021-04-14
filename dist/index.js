d16f37c3cfb865d5c7c4a2758c7f9d0cf700adb5
martina <martina.bevilacqua1992@gmail.com>
2021-04-14 17:11:10 +0200

    upload dist folder

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.SequenceViewer = void 0;
+var sequence_viewer_1 = require("./lib/sequence.viewer");
+Object.defineProperty(exports, "SequenceViewer", { enumerable: true, get: function () { return sequence_viewer_1.SequenceViewer; } });
